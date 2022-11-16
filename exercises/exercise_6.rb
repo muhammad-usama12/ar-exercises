982c2aaad69bf498bd4a2f95e4a0f028d4d27e75
your_github_username <muhammad.usama12@hotmail.com>
2022-11-16 01:32:02 +0000

    complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store2 = Store.find(2)
+@store2.employees.create(first_name: "Muhammad", last_name: "Usama", hourly_rate: 50)
+
+@store3 = Store.find(4)
+@store3.employees.create(first_name: "Aayan", last_name: "Yaseen", hourly_rate: 55)
+
