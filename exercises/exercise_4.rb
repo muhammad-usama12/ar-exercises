42273ba3cd5ffa208fe345d406a529737e1826d9
your_github_username <muhammad.usama12@hotmail.com>
2022-11-16 00:59:15 +0000

    complete exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |i| 
+  puts "Store Name that carries men's apparel: #{i.name}"
+end
+
+@womens_stores = Store.where("annual_revenue < 1000000 and womens_apparel = true")
+
+@womens_stores.each do |i| 
+  puts "Store Name #{i.name} Annual Revenue #{i.annual_revenue}"
+end
