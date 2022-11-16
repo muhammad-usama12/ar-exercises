beb6aa3bfd6fb03ffc17cf9eb5bffbbb70965504
your_github_username <muhammad.usama12@hotmail.com>
2022-11-16 01:18:18 +0000

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@store_sum = Store.sum(:annual_revenue)
+@store_rev_avg = Store.average(:annual_revenue)
+@store_standing = Store.where("annual_revenue > 1000000").size
+
+
+
+
+puts @store_sum
+puts @store_rev_avg
+puts @store_standing
